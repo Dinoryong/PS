d34f8d512072de9972a27bc5fa571fa749abce33
구영지 <biiregrey2@gmail.com>
2021-01-08 23:25:25 +0900

    0108ps

--- 0_My_templates/My Python Sheet.md
+++ 0_My_templates/My Python Sheet.md
@@ -8,3 +8,8 @@
 
 
 
+divmond
+
+
+
+unpacking
--- pg/pg_pythonic1.py
+++ pg/pg_pythonic1.py
@@ -1,7 +1,11 @@
-
+# my answer -> more like c, java
 def solution(mylist):
     answer = []
     for i in range(len(mylist)):
         j = len(mylist[i])
         answer.append(j)
     return answer
+
+# pythonic
+def solution(mylist):
+    return list(map(len, mylist))
