81e086aa7d23124c9613bf743596ebd2135e9734
Dinoryong <biiregrey2@gmail.com>
2022-02-10 01:54:22 +0900

    Update : update lc codes

--- leetcode/704. Binary Search.py
+++ leetcode/704. Binary Search.py
@@ -9,3 +9,30 @@ class Solution:
             else:
                 continue
         return -1
+
+'''
+simpler
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+	        return nums.index(target)
+        else:
+	        return -1
+
+''' 
+Binary Search
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start<=end:
+            mid = (end + start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid]>target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
