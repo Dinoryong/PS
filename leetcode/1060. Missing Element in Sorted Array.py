5238feb7cc1da2616774a0f262f9e6d16c1753c2
Dinoryong <biiregrey2@gmail.com>
2022-02-26 23:53:31 +0900

    Update : update ps

--- leetcode/1060. Missing Element in Sorted Array.py
+++ leetcode/1060. Missing Element in Sorted Array.py
@@ -1,6 +1,7 @@
 # Runtime Error
 '''
 IndexError: list index out of range
+    return temp[k-1]
 
 '''
 class Solution:
@@ -11,3 +12,23 @@ class Solution:
         for i in range(len(nums)):
             temp.remove(nums[i])
         return temp[k-1]
+
+'''
+시도.. 
+'''
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        temp = [0]
+        for i in range(1, len(nums)):
+            temp.append((nums[i] - nums[i - 1] - 1))
+        # print(temp)
+        while k > 0:
+            for i in range(len(temp)):
+                if temp[i] == 0:
+                    pass
+                else:
+                    k -= temp[i]
+
+''' 
+
+'''
