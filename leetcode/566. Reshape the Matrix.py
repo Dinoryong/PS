99a7e9ac01e61f1a01e50e672cca6fc3a5934fc4
Dinoryong <biiregrey2@gmail.com>
2022-03-02 18:25:11 +0900

    Update : update ps

---
+++ leetcode/566. Reshape the Matrix.py
@@ -0,0 +1,19 @@
+'''
+array[start:end-1] 은 list 형태로 보여준다
+'''
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        temp = []
+        matrix = []
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                temp.append(mat[i][j])
+        # print(temp)
+        if r * c == len(temp):
+            for i in range(0, len(temp), c):
+                matrix.append(temp[i:i + c])
+            return matrix
+
+        # otherwise return the original matrix
+        else:
+            return mat
