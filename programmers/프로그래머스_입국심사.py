045d545bf122aef11e3eddd01fa5309188b8baae
Dinoryong <biiregrey2@gmail.com>
2022-03-13 23:37:02 +0900

    Update : update ps

---
+++ programmers/프로그래머스_입국심사.py
@@ -0,0 +1,18 @@
+def solution(n, times):
+    answer = 0
+    left, right = 1, max(times) * n
+
+    while left < right:
+        mid = (left + right) // 2
+        total = 0
+
+        for t in times:
+            total += mid // t
+
+        if total >= n:
+            right = mid
+        else:
+            left = mid + 1
+
+    answer = left
+    return answer
